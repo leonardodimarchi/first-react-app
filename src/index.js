0c0c181358ae659465b8972a2e769d25de6c87cf
leonardo <leonardodimarchi@gmail.com>
2021-02-25 19:05:55 -0300

    adding all the files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+const message = {
+    date: new Date(),
+    text: "i'm loving this",
+    author: {
+        name: 'Luiz Toquetto',
+        avatar_img: 'https://placekitten.com/200/200',
+    }
+}
+
+function Message(props){
+    return(
+        <div className="Message">
+            <UserInfo user={props.user.author}/>
+            <div className="MessageText">
+                {props.user.text}
+            </div>
+            <div className="FormatedDate">
+                {formatDate(props.user.date)}
+            </div>
+        </div>
+    )
+}
+
+function UserInfo(props){
+    return(
+        <div className="UserInfo">
+            <Avatar user={props.user}/>
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    )
+}
+
+function Avatar(props){
+    return(
+        <img className="Avatar" 
+            src={props.user.avatar_img} 
+            alt={props.user.name}>
+        </img>
+    )
+}
+
+function formatDate(date){
+    return date.toLocaleDateString()
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Message user={message}/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
